e60110344c22e7f5a564f7bd7801a683f99b412a
Rehan <rehan@Rehans-MBP.lan>
2025-08-13 17:53:05 +0530

    First Render Test

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const rectangle = document.getElementById('rectangle');
 const button = document.getElementById('cutButton');
 const square = document.querySelector('.center-square');
+const aboutme = document.querySelector('.aboutme');
+const projects = document.querySelector('.projects');
+const contact = document.querySelector('.contact');
 
 button.addEventListener('click', () => {
-    rectangle.style.width = '40%';
+    rectangle.style.width = '30%';
     square.style.width = '100%';
     square.style.height = '100%';
+    projects.style.left = '40vw';
+    aboutme.style.left = '40vw';
+    contact.style.left = '40vw';
     button.style.display = 'none';
 });
