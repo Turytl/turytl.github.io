ba8d8871105843fc2a917bbd9ba89b58fa943bb6
Rehan <rehanalyze@gmail.com>
2025-08-21 22:38:59 +0530

    full website refresh + old delete, new design

---
+++ script.js
@@ -0,0 +1,19 @@
+particlesJS("background", {
+  particles: {
+    number: { value: 150, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 1, random: true },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: { enable: true, speed: 2, random: true, out_mode: "out" },
+  },
+  interactivity: { detect_on: "window", events: { resize: true } },
+  retina_detect: true,
+});
